a3864e4054240292c8e371e39d189f724b7069a7
FadeZL <1136107056@qq.com>
2019-06-24 16:53:21 +0800

    完善登陆代码

--- src/main/java/life/majiang/community/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/community/controller/AuthorizeController.java
@@ -1,4 +1,31 @@
 package life.majiang.community.community.controller;
 
+import life.majiang.community.community.dto.AccessTokenDTO;
+import life.majiang.community.community.dto.GithubUser;
+import life.majiang.community.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id("c7644f05289c3c20778d");
+        accessTokenDTO.setClient_secret("963b0744f8b9a2bf881e01fd9a8834b70eb475ca");
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
 }
--- src/main/java/life/majiang/community/community/dto/GithubUser.java
+++ src/main/java/life/majiang/community/community/dto/GithubUser.java
@@ -1,4 +1,32 @@
 package life.majiang.community.community.dto;
 
 public class GithubUser {
+
+    private Long id;
+    private String name;
+    private String boi;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBoi() {
+        return boi;
+    }
+
+    public void setBoi(String boi) {
+        this.boi = boi;
+    }
 }
